d1a4fc359f10b48b35c9a8dab2379248544cc217
vishal <drcyberx@gmail.com>
2020-11-30 08:48:45 +0530

    removing callback from listen

--- app.js
+++ app.js
@@ -75,6 +75,4 @@ app.get('/', (req, res) => {
    });
 });
 
-app.listen(process.env.PORT || port, () => {
-   // console.log(`The server is running at http://${hostname}:${port}`);
-});
+app.listen(process.env.PORT || port);
