d51f501f1fd0f21d6ed617a2e62e4b6d65e2ae41
vishal <drcyberx@gmail.com>
2020-12-05 19:29:46 +0530

    adding random cities

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const https = require('https');
 const { response } = require('express');
-const { hostname } = require('os');
+const ejs = require('ejs');
+// const { hostname } = require('os');
 
-// const hostname = '127.0.0.1';
+const hostname = '127.0.0.1';
 const port = 3000;
 
 // Initialize app here
@@ -15,16 +16,21 @@ app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use('/public', express.static('public'));
 
-var worldCities = []
-function randomCity(){
-   let x = Math.floor((Math.random() * 100) + 1);
+var worldCities = ['london', 'delhi', 'mumbai', 'Kolkata', 'Chennai', 'banglore', 'hyderabad', 'New York', 'Los Angeles', 'Chicago', 'Phoenix', 'Boston', 'Washington dc', 'Liverpool', 'Paris', 'Strasbourg', 'Sydney', 'Melbourne', 'Perth', 'Canberra', 'Moscow', 'Kyoto', 'Osaka', 'Hiroshima', 'dhaka', 'islamabad', 'kabul', 'cape town', 'winden', 'Berlin', 'Hamburg']
+
+function randomCity() {
+   let x = Math.floor((Math.random() * worldCities.length) + 1);
    return x;
 }
 
-const url0 = 'https://api.openweathermap.org/data/2.5/weather?q=london&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
-const url1 = 'https://api.openweathermap.org/data/2.5/weather?q=delhi&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
-const url2 = 'https://api.openweathermap.org/data/2.5/weather?q=paris&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
-const url3 = 'https://api.openweathermap.org/data/2.5/weather?q=auckland&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
+console.log(randomCity());
+
+const url0 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
+const url1 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
+const url2 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
+const url3 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
+
+
 
 let city0 = '';
 let city1 = '';
@@ -33,31 +39,33 @@ let city3 = '';
 
 // getting data with api url assined above
 https.get(url0, (response0) => {
-   response0.on('data', (data) => {
-      city0 = JSON.parse(data);
+   response0.on('data', (data0) => {
+      city0 = JSON.parse(data0);
    });
 });
 
 https.get(url1, (response1) => {
-   response1.on('data', (data) => {
-      city1 = JSON.parse(data);
+   response1.on('data', (data1) => {
+      city1 = JSON.parse(data1);
    });
 });
 
 https.get(url2, (response2) => {
-   response2.on('data', (data) => {
-      city2 = JSON.parse(data);
+   response2.on('data', (data2) => {
+      city2 = JSON.parse(data2);
    });
 });
 
 https.get(url3, (response3) => {
-   response3.on('data', (data) => {
-      city3 = JSON.parse(data);
+   response3.on('data', (data3) => {
+      city3 = JSON.parse(data3);
    });
 });
 
 // Making get request on home  route
 app.get('/', (req, res) => {
+
+
    res.render('home', {
 
       // City name here
@@ -91,10 +99,10 @@ app.get('/', (req, res) => {
       windspeed4: city3.wind.speed + 'km/hr',
 
       // WEather icon here
-      weatherIcon0 : city0.weather[0].icon,
-      weatherIcon1 : city1.weather[0].icon,
-      weatherIcon2 : city2.weather[0].icon,
-      weatherIcon3 : city3.weather[0].icon,
+      weatherIcon0: city0.weather[0].icon,
+      weatherIcon1: city1.weather[0].icon,
+      weatherIcon2: city2.weather[0].icon,
+      weatherIcon3: city3.weather[0].icon,
 
    });
 });
@@ -111,7 +119,7 @@ app.post('/', function (req, res) {
             customCityMaxTemp: customCityData.main.temp_max + ' *C',
             customCityMinTemp: customCityData.main.temp_min + ' *C',
             customCityWindSpeed: customCityData.wind.speed + 'km/hr',
-            CustomWeatherIcon : customCityData.weather[0].icon
+            CustomWeatherIcon: customCityData.weather[0].icon
          });
       });
    });
@@ -119,6 +127,6 @@ app.post('/', function (req, res) {
 
 
 // Litening app here
-app.listen(process.env.PORT || port,  () => {
-   // console.log(`The server is running at http://${hostname}:${port}`);
+app.listen(process.env.PORT || port, hostname, () => {
+   console.log(`The server is running at http://${hostname}:${port}`);
 });
