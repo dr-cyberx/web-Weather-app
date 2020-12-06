19266fc51f1928656358599816513c5abd653497
vishal <drcyberx@gmail.com>
2020-12-06 10:11:34 +0530

    adding ranom city urls in get route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyparser = require('body-parser');
 const https = require('https');
 const { response } = require('express');
 const ejs = require('ejs');
+const randomNumber = require(__dirname + '/randomNum.js');
 // const { hostname } = require('os');
 
 const hostname = '127.0.0.1';
@@ -18,55 +19,48 @@ app.use('/public', express.static('public'));
 
 var worldCities = ['london', 'delhi', 'mumbai', 'Kolkata', 'Chennai', 'banglore', 'hyderabad', 'New York', 'Los Angeles', 'Chicago', 'Phoenix', 'Boston', 'Washington dc', 'Liverpool', 'Paris', 'Strasbourg', 'Sydney', 'Melbourne', 'Perth', 'Canberra', 'Moscow', 'Kyoto', 'Osaka', 'Hiroshima', 'dhaka', 'islamabad', 'kabul', 'cape town', 'winden', 'Berlin', 'Hamburg']
 
-function randomCity() {
-   let x = Math.floor((Math.random() * worldCities.length) + 1);
-   return x;
-}
-
-console.log(randomCity());
-
-const url0 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
-const url1 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
-const url2 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
-const url3 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
-
-
 
 let city0 = '';
 let city1 = '';
 let city2 = '';
 let city3 = '';
 
-// getting data with api url assined above
-https.get(url0, (response0) => {
-   response0.on('data', (data0) => {
-      city0 = JSON.parse(data0);
-   });
-});
-
-https.get(url1, (response1) => {
-   response1.on('data', (data1) => {
-      city1 = JSON.parse(data1);
-   });
-});
-
-https.get(url2, (response2) => {
-   response2.on('data', (data2) => {
-      city2 = JSON.parse(data2);
-   });
-});
-
-https.get(url3, (response3) => {
-   response3.on('data', (data3) => {
-      city3 = JSON.parse(data3);
-   });
-});
 
 // Making get request on home  route
 app.get('/', (req, res) => {
 
+   let url0 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomNumber.randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
+   let url1 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomNumber.randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
+   let url2 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomNumber.randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
+   let url3 = 'https://api.openweathermap.org/data/2.5/weather?q=' + worldCities[randomNumber.randomCity()] + '&appid=027df142e5b67a54015b3e224ea626cb&units=metric';
 
-   res.render('home', {
+
+   https.get(url0, (response0) => {
+      response0.on('data', (data0) => {
+         city0 = JSON.parse(data0);
+      });
+   });
+
+   https.get(url1, (response1) => {
+      response1.on('data', (data1) => {
+         city1 = JSON.parse(data1);
+      });
+   });
+
+   https.get(url2, (response2) => {
+      response2.on('data', (data2) => {
+         city2 = JSON.parse(data2);
+      });
+   });
+
+   https.get(url3, (response3) => {
+      response3.on('data', (data3) => {
+         city3 = JSON.parse(data3);
+      });
+   });
+
+   let source =
+   {
 
       // City name here
       mycity1: city0.name,
@@ -104,7 +98,9 @@ app.get('/', (req, res) => {
       weatherIcon2: city2.weather[0].icon,
       weatherIcon3: city3.weather[0].icon,
 
-   });
+   }
+
+   res.render('home', source);
 });
 
 app.post('/', function (req, res) {
---
+++ randomNum.js
@@ -0,0 +1,5 @@
+
+module.exports.randomCity = function () {
+   let x = Math.floor((Math.random() * 30) + 1);
+   return x;
+}
